3680f33f92f4b94a58dfc95345b19c2c2ee8cb02
Vaishakh <vaishakhsm@gmail.com>
2021-03-25 00:58:35 +0530

    Navbar changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,26 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {Navbar, Form, Nav, Button, FormControl} from 'react-bootstrap';
 
 const NavBar = () => {
   return (
-    <div className="navbar primary" style={{ opacity:0.4 }}>
-      <Link to="/" style={{ color: "black" }}>
-        HOME
-      </Link>
-      <Link to="/FarmerPage" style={{ color: "black" }}>
-        FARMER
-      </Link>
-      <Link to="/OfficialPage" style={{ color: "black" }}>
-        GOVERNMENT OFFICIAL
-      </Link>
-      <Link to="/PolicyMakerPage" style={{ color: "red" }}>
-        POLICY MAKER
-      </Link>
-
-      <Link to="/login" style={{ color: "red" }}>
-        Login
-      </Link>
-    </div>
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="/">Farmer DaPP</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="/FarmerPage">Farmer Registration</Nav.Link>
+      <Nav.Link href="/OfficialPage">Official Page</Nav.Link>
+    </Nav>
+    <Nav>
+      <Nav.Link href ="/login">Login</Nav.Link>
+    </Nav>
+  </Navbar>
   );
 };
 
